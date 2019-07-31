fbca5172bd81b7c6916142274190bbeeaa4b9a26
Austin4002 <326140305@qq.com>
2019-07-31 17:41:43 +0800

    登录注册

--- src/main/java/controller/UserControl.java
+++ src/main/java/controller/UserControl.java
@@ -1,14 +1,17 @@
 package controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import pojo.Result;
 import pojo.User;
 import service.UserService;
 
-@RequestMapping("/")
+@RequestMapping("/user")
 @RestController
 public class UserControl {
 	@Autowired
@@ -17,16 +20,26 @@ public class UserControl {
 	// 注册
 	@RequestMapping(value = "/register", method = RequestMethod.POST)
 	public Object userIndex(User user) {
-		userService.insert(user);
-		return new Result<>();
+		int i = userService.insert(user);
+		Result rs = new Result<>();
+		if(i>0) {
+			rs.setCode(200);
+			rs.setMsg("OK");
+		}else {
+			rs.setCode(-1);
+			rs.setMsg("注册失败");
+			
+		}
+		return rs;
 	}
 
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	public Object userLogin(String userAccount, String userPassword) {
+	public Object userLogin(String userAccount, String userPassword, HttpSession session) {
 		User user = userService.selectByuserAccount(userAccount);
-		Result result = new Result(-1, "错误");
+		Result result = new Result(-1, "登录失败");
 		if (user.getUserPassword().equals(userPassword)) {
-			result = new Result<>();
+			result = new Result<>(200, "OK");
+			session.setAttribute("user", user);
 		}
 		return result;
 	}
